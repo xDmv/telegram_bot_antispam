203107d16d56b92be574f1e617ac55b518398c90
Dima <makushin.dima@gmail.com>
2019-09-16 18:02:24 +0300

    up: Relice 1.0.3

--- bot.py
+++ bot.py
@@ -11,77 +11,77 @@ user0 = config.admin_user #берем список администраторо�
 
 try:
 	@bot.message_handler(content_types=['document'])
-	def delete_links(message):
+	def delete_links_document(message):
 		if (str(message.from_user.id) not in user0):
 			f = open('log.txt', 'a')
 			f.write(str(datetime.datetime.now()) + ' user id:' + str(message.from_user.id) + ' del message content_types=[document] ' + '\n')
 			f.close()
 			bot.delete_message(message.chat.id, message.message_id)
 	@bot.message_handler(content_types=['audio'])
-	def delete_links(message):
+	def delete_links_audio(message):
 		if (str(message.from_user.id) not in user0):
 			f = open('log.txt', 'a')
 			f.write(str(datetime.datetime.now()) + ' user id:' + str(message.from_user.id) + ' del message content_types=[audio] ' + '\n')
 			f.close()
 			bot.delete_message(message.chat.id, message.message_id)
 	@bot.message_handler(content_types=['video'])
-	def delete_links(message):
+	def delete_links_video(message):
 		if (str(message.from_user.id) not in user0):
 			f = open('log.txt', 'a')
 			f.write(str(datetime.datetime.now()) + ' user id:' +str(message.from_user.id) + ' del message content_types=[video] ' + '\n')
 			f.close()
 			bot.delete_message(message.chat.id, message.message_id)
 	@bot.message_handler(content_types=['sticker'])
-	def delete_links(message):
+	def delete_links_sticker(message):
 		if (str(message.from_user.id) not in user0):
 			f = open('log.txt', 'a')
 			f.write(str(datetime.datetime.now()) + ' user id:' + str(message.from_user.id) + ' del message  content_types=[sticker] ' + '\n')
 			f.close()
 			bot.delete_message(message.chat.id, message.message_id)
 	@bot.message_handler(content_types=['location'])
-	def delete_links(message):
+	def delete_links_location(message):
 		if (str(message.from_user.id) not in user0):
 			f = open('log.txt', 'a')
 			f.write(str(datetime.datetime.now()) + ' user id:' +str(message.from_user.id) + ' del message content_types=[location] ' + '\n')
 			f.close()
 			bot.delete_message(message.chat.id, message.message_id)
 	@bot.message_handler(content_types=['contact'])
-	def delete_links(message):
+	def delete_links_contact(message):
 		if (str(message.from_user.id) not in user0):
 			f = open('log.txt', 'a')
 			f.write(str(datetime.datetime.now()) + ' user id:' + str(message.from_user.id) + ' del message content_types=[contact] ' + '\n')
 			f.close()
 			bot.delete_message(message.chat.id, message.message_id)
 	@bot.message_handler(content_types=['caption'])
-	def delete_links(message):
+	def delete_links_caption(message):
 		if (str(message.from_user.id) not in user0):
 			f = open('log.txt', 'a')
 			f.write(str(datetime.datetime.now()) + ' user id:' + str(message.from_user.id) + ' del message content_types=[caption] ' + '\n')
 			f.close()
 			bot.delete_message(message.chat.id, message.message_id)
 	@bot.message_handler(content_types=['venue'])
-	def delete_links(message):
+	def delete_links_venue(message):
 		if (str(message.from_user.id) not in user0):
 			f = open('log.txt', 'a')
 			f.write(str(datetime.datetime.now()) + ' user id:' + str(message.from_user.id) + ' del message content_types=[venue] ' + '\n')
 			f.close()
 			bot.delete_message(message.chat.id, message.message_id)
 	@bot.message_handler(content_types=['photo'])
-	def delete_links(message):
+	def delete_links_photo(message):
 		if (str(message.from_user.id) not in user0):
 			f = open('log.txt', 'a')
 			f.write(str(datetime.datetime.now()) + ' user id:' + str(message.from_user.id) + ' del message content_types=[photo] ' + '\n')
 			f.close()
 			bot.delete_message(message.chat.id, message.message_id)
 	@bot.message_handler(content_types=['left_chat_member'])
-	def delete_links(message):
+	def delete_links_left_chat_member(message):
 		if (str(message.from_user.id) not in user0):
 			f = open('log.txt', 'a')
 			f.write(str(datetime.datetime.now()) + ' user id:' + str(message.from_user.id) + ' del message content_types=[left_chat_member] ' + '\n')
 			f.close()
 			bot.delete_message(message.chat.id, message.message_id)
 	@bot.message_handler(content_types=['new_chat_members'])
-	def delete_links(message):
+	def delete_links_new_chat_members(message):
 		if (str(message.from_user.id) not in user0):
 			f = open('log.txt', 'a')
 			f.write(str(datetime.datetime.now()) + ' user id:' + str(message.from_user.id) + ' del message content_types=[new_chat_members] ' + '\n')
@@ -98,6 +98,16 @@ try:
 					if (entity.type in ["url"]):
 						f = open('log.txt', 'a')
 						f.write(str(datetime.datetime.now()) + ' user id:' + str(message.from_user.id) + " del message type == 'url'" + '\n')
+						f.close()
+						bot.delete_message(message.chat.id, message.message_id)
+					if (entity.type in ["text_link"]):
+						f = open('log.txt', 'a')
+						f.write(str(datetime.datetime.now()) + ' user id:' + str(message.from_user.id) + " del message type == 'text_link'" + '\n')
+						f.close()
+						bot.delete_message(message.chat.id, message.message_id)
+					if (entity.type in ["pre"]):
+						f = open('log.txt', 'a')
+						f.write(str(datetime.datetime.now()) + ' user id:' + str(message.from_user.id) + " del message type == 'pre'" + '\n')
 						f.close()
 						bot.delete_message(message.chat.id, message.message_id)
 					if entity.type in ["hashtag"]:
@@ -125,13 +135,23 @@ try:
 						f.write(str(datetime.datetime.now()) + ' user id:' + str(message.from_user.id) + " del message type == 'bold'" + '\n')
 						f.close()
 						bot.delete_message(message.chat.id, message.message_id)
+					if entity.type in ["mention"]:
+						f = open('log.txt', 'a')
+						f.write(str(datetime.datetime.now()) + ' user id:' + str(message.from_user.id) + " del message type == 'mention'" + '\n')
+						f.close()
+						bot.delete_message(message.chat.id, message.message_id)
+					if (entity.type in ["italic"]):
+						f = open('log.txt', 'a')
+						f.write(str(datetime.datetime.now()) + ' user id:' + str(message.from_user.id) + " del message type == 'italic'" + '\n')
+						f.close()
+						bot.delete_message(message.chat.id, message.message_id)
 
 	@bot.message_handler(content_types=["text"])
 	def repeat_all_messages(message):
 		if (str(message.from_user.id) not in user0):
 			if message.forward_from_chat :
 				f = open('log.txt', 'a')
-				f.write(str(datetime.datetime.now()) + ' user id:' + str(message.from_user.id) + ' del message message with buttons ' + '\n')
+				f.write(str(datetime.datetime.now()) + ' user id:' + str(message.from_user.id) + ' del message with buttons ' + '\n')
 				f.close()
 				bot.delete_message(message.chat.id, message.message_id)
 			else:
